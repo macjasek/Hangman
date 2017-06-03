04020f79e338c3fb29c43cfdd196dc1c2c735927
macjasek <maciej.dobruchowski@ispro.com.pl>
2017-06-03 13:13:24 +0200

    Dodana klasa Players (in progress)

---
+++ HangMan/PlayerList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace HangMan
+{
+    class PlayerList
+    {
+        Dictionary<string, int> PlayersList {get; set;}
+        public PlayerList()
+        {
+            var path = @".\PlayerList.txt";
+            if (File.Exists(path))
+            {
+                //Wczytaj listę graczy
+
+            }
+            else
+            {
+                File.Create(path);
+            }
+        }
+    }
+}
